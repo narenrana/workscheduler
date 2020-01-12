fe684aedc47504e762196813493f4059ea48b46d
Narender <narenrana02@gmail.com>
2020-01-12 11:39:08 +0800

    Remove description

--- src/workinghour.js
+++ src/workinghour.js
@@ -1,26 +1,3 @@
-/*
-# Objective:
-write a function to find expiry datetime. expiry datetime is 3 working hours from "now".
-the working hours is defined in "schedule" input parameter.
-You can write the function in java or javascript.
-# input parameters:
-now: datetime, current datetime. e.g: '2019-10-11T08:13:07+0800'
-schedule: an arraylist of map object. which specified the day open or close and also the start and end of working hours
-[
-	{"open": false, "open_at": "", close_at: ""}, // sunday
-	{"open": true, "open_at": "09:00", close_at: "18:00"}, // monday
-	{"open": true, "open_at": "09:00", close_at: "18:00"},
-	{"open": true, "open_at": "09:00", close_at: "18:00"},
-	{"open": true, "open_at": "09:00", close_at: "18:00"},
-	{"open": true, "open_at": "09:00", close_at: "17:00"},
-	{"open": false, "open_at": "", close_at: ""},
-]
-# example:
-now is friday 4pm. with the above schedule, the expiry date should be next monday 10am. because on friday office close
-at 5pm and office is closed on weekend.
-output: datetime, 3 working hour from input date ("now")
-*/
-
 const SCHEDULE_EXPIRE_TIME=3;
 
 /**
